90ec24c1cf85a6011b5a1097df5da5d361feead0
nngocjade <nngocjade@gmail.com>
2021-03-02 15:55:16 +0700

    added test-email route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const email = require("../helpers/email");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* Temporary GET route to send myself an email. */
+router.get("/test-email", (req, res) => {
+  email.sendTestEmail();
+  res.send("email sent");
 });
 
 module.exports = router;
