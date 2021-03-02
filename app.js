75b0ff0ebe0ec29ad13b799a0fe7e16610ce07ff
nngocjade <nngocjade@gmail.com>
2021-03-02 17:29:02 +0700

    installed mongoose and reinstall nodemon

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
-
+const mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+mongoose
+  .connect("mongodb://localhost/email", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB database connection established successfully!");
+  })
+  .catch((err) => console.log("Could not connect to database!", err));
 
 app.use(logger("dev"));
 app.use(express.json());
