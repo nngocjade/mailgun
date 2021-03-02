c397bdacb248ee89023167c4072eb0e716f6dbf5
nngocjade <nngocjade@gmail.com>
2021-03-02 15:55:43 +0700

    installed nodemon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
